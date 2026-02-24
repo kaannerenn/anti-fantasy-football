e52704f7b2afab3fe6b45b2d96d5b020265c8e73
kaannerenn <yavuzk20@itu.edu.tr>
2026-02-24 21:58:09 +0300

    thesportsdb' API'ı kullanılarak örnek bir veri seti çekilmiştir. Oyuncuların istatistikleri random olarak simüle edilmiştir.

--- league/urls.py
+++ league/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('player/<int:pk>/',views.player_detail, name='player_detail'),
     path('register/',views.register,name="register"),
     path('login/',auth_views.LoginView.as_view(template_name='league/login.html',authentication_form=LoginFormu),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(next_page='player_list'),name='logout'),
+    path('logout/',auth_views.LogoutView.as_view(next_page='login'),name='logout'),
     path('add-to-team/<int:player_id>/', views.add_to_team, name='add_to_team'),
     path('my-squad/',views.my_squad,name='my_squad'),
 ]
